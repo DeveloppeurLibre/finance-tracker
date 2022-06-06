0abc8ef83c71b27d7d5f9b98580a7390c3bab19c
DeveloppeurLibre <contact@developpeurlibre.com>
2022-06-06 14:57:29 +0200

    Add IconSelector cell

--- FinanceTracker/AccountCreationView.swift
+++ FinanceTracker/AccountCreationView.swift
@@ -10,25 +10,35 @@ import SwiftUI
 struct AccountCreationView: View {
 	
 	@State private var amount: String = ""
+	@State private var selectedIcon: String = "icon_001"
 	@State private var selectedCurrency: Currency = .euro
 	
     var body: some View {
-		VStack(alignment: .leading) {
-			Text("Solde initial")
-				.font(.title2)
-				.bold()
-			HStack {
-				TextField("Ex : 486,73 \(selectedCurrency.rawValue)", text: $amount)
-					.keyboardType(.numbersAndPunctuation)
-					.submitLabel(.done)
-					.padding(12)
-					.padding(.horizontal, 12)
-				CurrencySelector(selectedCurrency: $selectedCurrency)
-					.foregroundColor(Color("purple"))
-					.padding(4)
+		VStack(spacing: 32) {
+			VStack(alignment: .leading) {
+				Text("Icône")
+					.font(.title2)
+					.bold()
+				IconSelector(selectedIcon: $selectedIcon)
+					.padding(.horizontal, -16)
 			}
-			.background(Color.white)
-			.cornerRadius(.infinity)
+			VStack(alignment: .leading) {
+				Text("Solde initial")
+					.font(.title2)
+					.bold()
+				HStack {
+					TextField("Ex : 486,73 \(selectedCurrency.rawValue)", text: $amount)
+						.keyboardType(.numbersAndPunctuation)
+						.submitLabel(.done)
+						.padding(12)
+						.padding(.horizontal, 12)
+					CurrencySelector(selectedCurrency: $selectedCurrency)
+						.foregroundColor(Color("purple"))
+						.padding(4)
+				}
+				.background(Color.white)
+				.cornerRadius(.infinity)
+			}
 		}
 		.padding()
 		.background(Color("grey"))
---
+++ FinanceTracker/IconSelector.swift
@@ -0,0 +1,59 @@
+//
+//  IconSelector.swift
+//  FinanceTracker
+//
+//  Created by Quentin Cornu on 06/06/2022.
+//
+
+import SwiftUI
+
+struct IconSelector: View {
+	
+	@Binding var selectedIcon: String
+	
+	private let icons = [
+		"icon_001",
+		"icon_002",
+		"icon_003",
+		"icon_004",
+		"icon_005",
+		"icon_006",
+		"icon_007"
+	]
+	
+	var body: some View {
+		ScrollView(.horizontal, showsIndicators: false) {
+			HStack {
+				ForEach(icons, id: \.self) { iconName in
+					Button {
+						selectedIcon = iconName
+					} label: {
+						Circle()
+							.frame(width: 65, height: 65)
+							.foregroundColor(iconName == selectedIcon ? .black : .white)
+							.overlay(
+								Image(iconName)
+									.resizable()
+									.renderingMode(.template)
+									.foregroundColor(iconName == selectedIcon ? .white : .black)
+									.frame(width: 35, height: 35)
+							)
+					}
+				}
+			}
+			.padding(.horizontal, 16)
+		}
+	}
+}
+
+struct IconSelector_Previews: PreviewProvider {
+	
+	@State static var previewSelectedIcon = "icon_001"
+	
+	static var previews: some View {
+		IconSelector(selectedIcon: $previewSelectedIcon)
+			.padding(.vertical)
+			.previewLayout(.sizeThatFits)
+			.background(Color("grey"))
+	}
+}
