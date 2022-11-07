a531b1f9c7761c8860b9cdabce1942c52072b219
DeveloppeurLibre <contact@developpeurlibre.com>
2022-11-07 15:22:56 +0100

    Add icons

--- FinanceTracker/AccountCreationView.swift
+++ FinanceTracker/AccountCreationView.swift
@@ -18,67 +18,81 @@ struct AccountCreationView: View {
 	var onAccountCreated: (Account) -> Void
 	
     var body: some View {
-		VStack(spacing: 32) {
-			VStack(spacing: 16) {
-				Text(accountName == "" ? "Nouveau compte" : accountName)
-					.font(.system(size: 32, weight: .bold))
-					.padding(.top, 32)
-				Text("Solde : \(String(format: "%.2f", Float(amount) ?? 0.0)) \(selectedCurrency.rawValue)")
-					.font(.system(size: 20, weight: .light))
-					.foregroundColor(Color.secondaryText)
-					.frame(maxWidth: .infinity)
-			}
-			.frame(maxWidth: .infinity)
-			VStack(alignment: .leading) {
-				Text("Nom")
-					.font(.title2)
-					.bold()
-				TextField("Ex : PayPal...", text: $accountName)
-					.submitLabel(.done)
-					.padding(12)
-					.padding(.horizontal, 12)
-					.background(Color.cellBackground)
-					.cornerRadius(.infinity)
-			}
-			VStack(alignment: .leading) {
-				Text("Icône")
-					.font(.title2)
-					.bold()
-				IconSelector(selectedIcon: $selectedIcon)
-					.padding(.horizontal, -16)
-			}
-			VStack(alignment: .leading) {
-				Text("Solde initial")
-					.font(.title2)
-					.bold()
-				HStack {
-					TextField("Ex : 486,73 \(selectedCurrency.rawValue)", text: $amount)
-						.keyboardType(.numbersAndPunctuation)
+		NavigationView {
+			VStack(spacing: 32) {
+				VStack(spacing: 16) {
+					Text(accountName == "" ? "Nouveau compte" : accountName)
+						.font(.system(size: 32, weight: .bold))
+						.padding(.top, 32)
+					Text("Solde : \(String(format: "%.2f", Float(amount) ?? 0.0)) \(selectedCurrency.rawValue)")
+						.font(.system(size: 20, weight: .light))
+						.foregroundColor(Color.secondaryText)
+						.frame(maxWidth: .infinity)
+				}
+				.frame(maxWidth: .infinity)
+				VStack(alignment: .leading) {
+					Text("Nom")
+						.font(.title2)
+						.bold()
+					TextField("Ex : PayPal...", text: $accountName)
 						.submitLabel(.done)
 						.padding(12)
 						.padding(.horizontal, 12)
-					CurrencySelector(selectedCurrency: $selectedCurrency)
-						.foregroundColor(Color("purple"))
-						.padding(4)
+						.background(Color.cellBackground)
+						.cornerRadius(.infinity)
 				}
-				.background(Color.cellBackground)
-				.cornerRadius(.infinity)
+				VStack(alignment: .leading) {
+					HStack {
+						Text("Icône")
+							.font(.title2)
+							.bold()
+						Spacer()
+						if #available(iOS 16.0, *) {
+							NavigationLink {
+								IconSelectorScreen()
+							} label: {
+								Text("Voir plus")
+							}
+						}
+					}
+					IconSelector(selectedIcon: $selectedIcon)
+						.padding(.horizontal, -16)
+				}
+				VStack(alignment: .leading) {
+					Text("Solde initial")
+						.font(.title2)
+						.bold()
+					HStack {
+						TextField("Ex : 486,73 \(selectedCurrency.rawValue)", text: $amount)
+							.keyboardType(.numbersAndPunctuation)
+							.submitLabel(.done)
+							.padding(12)
+							.padding(.horizontal, 12)
+						CurrencySelector(selectedCurrency: $selectedCurrency)
+							.foregroundColor(Color("purple"))
+							.padding(4)
+					}
+					.background(Color.cellBackground)
+					.cornerRadius(.infinity)
+				}
+				Spacer()
+				MainButton(title: "Créer") {
+					let newAccount = Account(
+						iconName: selectedIcon,
+						name: accountName,
+						initialAmount: Float(amount) ?? 0,
+						transactions: [],
+						currency: selectedCurrency
+					)
+					onAccountCreated(newAccount)
+					presentationMode.wrappedValue.dismiss()
+				}
 			}
-			Spacer()
-			MainButton(title: "Créer") {
-				let newAccount = Account(
-					iconName: selectedIcon,
-					name: accountName,
-					initialAmount: Float(amount) ?? 0,
-					transactions: [],
-					currency: selectedCurrency
-				)
-				onAccountCreated(newAccount)
-				presentationMode.wrappedValue.dismiss()
-			}
+			.padding()
+			.background(Color.appBackground)
+			.navigationTitle(Text("Nouveau compte"))
+			.navigationBarHidden(true)
 		}
-		.padding()
-		.background(Color.appBackground)
     }
 }
 
---
+++ FinanceTracker/IconSelectorScreen.swift
@@ -0,0 +1,117 @@
+//
+//  IconSelectorScreen.swift
+//  FinanceTracker
+//
+//  Created by Quentin Cornu on 07/11/2022.
+//
+
+import PhotosUI
+import SwiftUI
+
+@available(iOS 16.0, *)
+struct IconSelectorScreen: View {
+	
+	@State private var selectedIcon: String = "icon_001"
+	@State private var selectedItems: [PhotosPickerItem] = []
+	@State private var selectedImagesData: [Data] = []
+	
+	private let icons = [
+		"icon_001",
+		"icon_002",
+		"icon_003",
+		"icon_004",
+		"icon_005",
+		"icon_006",
+		"icon_007",
+		"icon_008",
+		"icon_009",
+		"icon_010",
+		"icon_012",
+		"icon_013",
+		"icon_014",
+		"icon_015",
+		"icon_016",
+		"icon_017",
+		"icon_018",
+		"icon_019",
+		"icon_020"
+	]
+	
+	
+	private let columns = [
+		GridItem(.flexible()),
+		GridItem(.flexible()),
+		GridItem(.flexible()),
+		GridItem(.flexible())
+	]
+	
+    var body: some View {
+		ScrollView {
+			Text("Icônes par défaut")
+				.font(.title2)
+				.bold()
+			LazyVGrid(columns: columns, spacing: 20) {
+				ForEach(icons, id: \.self) { iconName in
+					Button {
+						selectedIcon = iconName
+					} label: {
+						Circle()
+							.frame(width: 65, height: 65)
+							.foregroundColor(iconName == selectedIcon ? Color.pillSelectedBackground : Color.pillBackground)
+							.overlay(
+								Image(iconName)
+									.resizable()
+									.renderingMode(.template)
+									.foregroundColor(iconName == selectedIcon ? .mainButtonText : .mainText)
+									.frame(width: 35, height: 35)
+							)
+					}
+				}
+			}
+			Text("Importer des icônes")
+				.font(.title2)
+				.bold()
+			PhotosPicker(selection: $selectedItems, maxSelectionCount: 1, matching: .images) {
+				Circle()
+					.frame(width: 65, height: 65)
+					.foregroundColor(Color.pillBackground.opacity(0.8))
+					.overlay(
+						Image(systemName: "plus")
+							.font(.system(size: 24, weight: .semibold))
+							.foregroundColor(.mainText)
+							.frame(width: 35, height: 35)
+					)
+			}
+			ForEach(selectedImagesData, id: \.self) { data in
+				if let data = data, let uiImage = UIImage(data: data) {
+					Image(uiImage: uiImage)
+						.resizable()
+						.frame(width: 65, height: 65)
+				}
+			}
+		}
+		.background(Color.appBackground)
+		.onChange(of: selectedItems) { newValue in
+			guard let item = newValue.first else { return }
+			item.loadTransferable(type: Data.self) { result in
+				switch result {
+					case .success(let data):
+						if let data {
+							self.selectedImagesData.append(data)
+						} else {
+							print("Data is nil")
+						}
+					case .failure(let failure):
+						print(failure)
+				}
+			}
+		}
+    }
+}
+
+@available(iOS 16.0, *)
+struct IconSelectorScreen_Previews: PreviewProvider {
+	static var previews: some View {
+        IconSelectorScreen()
+    }
+}
